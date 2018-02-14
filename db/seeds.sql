50f73f3e59187fc0ce824f24bfc01ca50be20409
Louella Landicho <louellalandicho@Louellas-MacBook-Pro.local>
2018-02-14 14:40:22 -0800

    initial upload

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+
+-- //  Seeds Burgers into Database
+
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("MOOYAH Cheeseburger", false, "2016-11-05 04:20:00"),
+  ("Cheddar Burger", false, "2016-11-05 04:20:01"),
+  ("Bacon Burger", false, "2016-11-05 04:20:02"),
+  ("Swiss Burger", false, "2016-11-05 16:20:00"),
+  ("Mushroom Burger", false, "2016-11-05 16:20:01");
---
+++ server.js
@@ -0,0 +1,4 @@
+// Node Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
